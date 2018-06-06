42b8daf1194b5107abce2856c398b71f71322f35
Fabien Crespel <fabien@crespel.net>
2018-06-06 22:43:39 +0200

    Web Manager: add user tokens management UI (refs #6393)

--- manager/src/main/resources/static/js/controllers/account.js
+++ manager/src/main/resources/static/js/controllers/account.js
@@ -5,15 +5,20 @@ angular
 	$scope.init = function() {
 		$scope.user = {};
 		$scope.passwordChange = {};
+		$scope.tokenRequest = {};
 		$scope.getUser();
+		$scope.getTokens();
 	};
 
 	$scope.getUser = function() {
-		$scope.user = AccountService.getUser();
+		AccountService.getUser(function(data) {
+			$scope.user = data;
+		});
 	};
 
 	$scope.saveUser = function() {
-		$scope.user = AccountService.saveUser($scope.user, function() {
+		AccountService.saveUser($scope.user, function(data) {
+			$scope.user = data;
 			$translate('USER_UPDATED').then(function(text) {
 				AlertService.showSuccess(text);
 			});
@@ -21,7 +26,7 @@ angular
 	};
 
 	$scope.changePassword = function() {
-		AccountService.changePassword($scope.passwordChange.oldPassword, $scope.passwordChange.newPassword, function() {
+		AccountService.changePassword({ oldPassword: $scope.passwordChange.oldPassword, newPassword: $scope.passwordChange.newPassword }, function() {
 			$translate('PASSWORD_UPDATED').then(function(text) {
 				AlertService.showSuccess(text);
 			});
@@ -29,7 +34,36 @@ angular
 			$scope.passwordForm.$setPristine();
 			$scope.passwordForm.$setUntouched();
 		});
-	}
+	};
+
+	$scope.getTokens = function() {
+		AccountService.getTokens(function(data) {
+			$scope.tokens = data;
+		});
+	};
+
+	$scope.generateToken = function() {
+		AccountService.generateToken($scope.tokenRequest, function(data) {
+			$scope.generatedToken = data;
+			$scope.tokenRequest = {};
+			$scope.tokenForm.$setPristine();
+			$scope.tokenForm.$setUntouched();
+			$scope.getTokens();
+		});
+	};
+
+	$scope.hideGeneratedToken = function() {
+		$scope.generatedToken = undefined;
+	};
+
+	$scope.revokeToken = function(id) {
+		AccountService.revokeToken({ id: id }, function() {
+			$translate('TOKEN_REVOKE_SUCCESS').then(function(text) {
+				AlertService.showSuccess(text);
+			});
+			$scope.getTokens();
+		});
+	};
 
 	$scope.init();
 
